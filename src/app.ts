c21de8801c3adbfd47d1dd9da9d025d8754e1b25
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-09-14 17:30:08 -0300

    feat: [req 04] Cria o endpoint POST /cars onde é possível cadastrar um carro novo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
+import 'express-async-errors';
 import express from 'express';
+import errorHandler from './middlewares/error';
+import carRouter from './routes/Car';
 
 const app = express();
+app.use(express.json());
+
+app.use(carRouter);
+
+app.use(errorHandler);
 
 export default app;
