85e4a7f78d40bf2f7b823d6d0cbbe5c722c432c9
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-09-16 11:59:58 -0300

    fix: Corrige erros de ESLint na rota de Motorcycles

--- src/routes/Motorcycle.ts
+++ src/routes/Motorcycle.ts
@@ -9,11 +9,12 @@ const motorcycle = new MotorcycleModel();
 const motorcycleService = new MotorcycleService(motorcycle);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
-route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
-route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
-// eslint-disable-next-line sonarjs/no-duplicate-string
-route.get('/motorcycles/:id', (req, res) => motorcycleController.readOne(req, res));
-route.put('/motorcycles/:id', (req, res) => motorcycleController.update(req, res));
-route.delete('/motorcycles/:id', (req, res) => motorcycleController.delete(req, res));
+const motorcycleEndpoint = '/motorcycles';
+
+route.post(motorcycleEndpoint, (req, res) => motorcycleController.create(req, res));
+route.get(motorcycleEndpoint, (req, res) => motorcycleController.read(req, res));
+route.get(`${motorcycleEndpoint}/:id`, (req, res) => motorcycleController.readOne(req, res));
+route.put(`${motorcycleEndpoint}/:id`, (req, res) => motorcycleController.update(req, res));
+route.delete(`${motorcycleEndpoint}/:id`, (req, res) => motorcycleController.delete(req, res));
 
 export default route;
